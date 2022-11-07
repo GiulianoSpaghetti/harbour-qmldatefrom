350b8f5378744e8e54ef47030939528724a5e2e0
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-11-07 21:48:29 +0100

    Add files via upload

---
+++ translations/harbour-qmldatefrom-de.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>Mein Cover</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>2022-10-11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>numerone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>QMLDateFrom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert the name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calculate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You met </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> about </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> days ago.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert the date</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Giulio Sorrentino</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>© 2022</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GPL v3 o, secondo il tuo parere, qualsiasi
+versione successiva</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Go to the program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
